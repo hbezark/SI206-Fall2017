bb9cf0d68ce099513f0383fa6f51a4bc8daeb554
Hana Bezark <hbezark@umich.edu>
2017-09-28 15:50:11 -0400

    Completed Lab 4.

---
+++ lab4.py
@@ -0,0 +1,10 @@
+import re
+
+with open('mbox-short.txt', "r") as data:
+	for line in data:
+		if re.findall('From', line):
+			print (line)
+			numbers = re.findall('[0-9]+', line)
+			print (numbers)
+			name = re.findall('\s(\S+)@\S+', line)
+			print (name)
